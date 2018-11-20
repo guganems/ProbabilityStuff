df9b8bbb2a0ab9aa9247fe724b1ca226ac161176
gnems <g_nemsveridze@cu.edu.ge>
2018-11-20 15:05:35 +0400

    Working on noorder norepetition combination

---
+++ src/Main.kt
@@ -0,0 +1,31 @@
+fun main(vararg args: String){
+    println("HELLO!")
+    println("Write \"HELP\" for info; Write \"EXIT\" for Exit")
+    var userMessageForNextAction: String
+
+    do {
+        userMessageForNextAction = readLine().toString()
+
+        when(userMessageForNextAction){
+            "HELP" -> help()
+            "NOORDER NOREPETITION" -> noOrderAndRepetition()
+        }
+
+    }while (userMessageForNextAction != "EXIT")
+}
+
+fun help(){
+    println("0. \"EXIT\" -- Exit.")
+    println("1. \"NOORDER NOREPETITION\" -- When order and repetition is not important.")
+}
+
+fun noOrderAndRepetition(){
+    println("Enter m & n where \'C[n]^{m}\'")
+    print("m = ")
+    var m = readLine()!!.toInt()
+    println()
+    print("n = ")
+    var n = readLine()!!.toInt()
+
+    
+}
